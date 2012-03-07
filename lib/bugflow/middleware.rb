3d971015b03807f3c9f01556af308918dd15ccdd
macbury <macbury@gmail.com>
2012-03-07 10:13:36 +0100

    fix

--- lib/bugflow/middleware.rb
+++ lib/bugflow/middleware.rb
@@ -8,24 +8,24 @@ module BugFlow
 
     def bind_notifications
       ActiveSupport::Notifications.subscribe /start_processing.action_controller/ do |name, start, finish, id, payload|
-        @request_monitor ||= BugFlow::Request.new
         @request_monitor.parse_payload(payload)
       end
 
       ActiveSupport::Notifications.subscribe /sql.active_record/ do |name, start, finish, id, payload|
-        @request_monitor.queries << BugFlow::Query.new(start, finish, payload)
+        @request_monitor.queries << BugFlow::Query.new(start, finish, payload) if @request_monitor 
       end
     
       ActiveSupport::Notifications.subscribe /!render_template.action_view/ do |name, start, finish, id, payload|
-        @request_monitor.views << BugFlow::View.new(start, finish, payload)
+        @request_monitor.views << BugFlow::View.new(start, finish, payload) if @request_monitor
       end
       ActiveSupport::Notifications.subscribe "process_action.action_controller" do
         @request_monitor.finish!
+        @request_monitor = nil
       end
     end
 
     def call(env)
-      @request_monitor ||= BugFlow::Request.new
+      @request_monitor = BugFlow::Request.new
       @request_monitor.env = env
       begin
         @app.call(env)
